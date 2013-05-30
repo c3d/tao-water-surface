4d77987168182e5981ef4d015e3db352330001d3
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2013-05-30 17:54:32 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~jerome/Work/tao

--- water_factory.cpp
+++ water_factory.cpp
@@ -107,15 +107,6 @@ void WaterFactory::render_callback(void *arg)
 }
 
 
-void WaterFactory::identify_callback(void *arg)
-// ----------------------------------------------------------------------------
-//   Identify callback: don't do anything
-// ----------------------------------------------------------------------------
-{
-    (void) arg;
-}
-
-
 void WaterFactory::delete_callback(void *arg)
 // ----------------------------------------------------------------------------
 //   Delete water name
@@ -132,8 +123,7 @@ Name_p WaterFactory::water_show(text name)
 {
     Water* water = instance()->water(name);
     water->update();
-    instance()->tao->AddToLayout2(WaterFactory::render_callback,
-                                 WaterFactory::identify_callback,
+    instance()->tao->addToLayout(WaterFactory::render_callback,
                                  strdup(name.c_str()),
                                  WaterFactory::delete_callback);
     return XL::xl_true;
